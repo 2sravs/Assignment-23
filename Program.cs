bc94251613aa05a07b5ebfb62993a3f19ce2ac01
2Sravs <sravani.chekuri1243#gmail.com>
2023-08-04 15:46:41 +0530

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MySharedLibrary;
+
+namespace MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            try
+            {
+                Console.WriteLine("Login Page");
+                LoginOperation.login();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!! " + ex.Message);
+            }
+            finally { Console.ReadKey(); }
+
+
+
+        }
+        }
+}
